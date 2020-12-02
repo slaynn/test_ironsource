8b7e31d2084a2e756d6e8c5bb7d7323460b57027
Quentin Desrame <quen2404@gmail.com>
2020-12-03 00:32:29 +0100

    wip qd

--- test_ironsouce_swift/ViewController.swift
+++ test_ironsouce_swift/ViewController.swift
@@ -14,27 +14,72 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        IronSource.initWithAppKey("b6bd7235")
+        IronSource.setRewardedVideoDelegate(self)
+        IronSource.setInterstitialDelegate(self)
+        
+        IronSource.initWithAppKey("e210f449", adUnits: [IS_REWARDED_VIDEO, IS_INTERSTITIAL])
         ISIntegrationHelper.validateIntegration()
+        IronSource.loadInterstitial();
     }
     
     func displayVideo(){
         NSLog("trigger displayVideo")
-        
-        IronSource.setRewardedVideoDelegate(self)
+        IronSource.showRewardedVideo(with: self)
     }
 
-    @IBAction func buttonClick(_ sender: Any) {
+    @IBAction func videoClick(_ sender: Any) {
         displayVideo();
     }
+    
+    func displayInterstitial(){
+        NSLog("trigger displayIntestitial %@", IronSource.hasInterstitial())
+        IronSource.showInterstitial(with: self)
+    }
+
+    @IBAction func interstitialClick(_ sender: Any) {
+        displayInterstitial();
+    }
+    
+}
+extension ViewController:ISInterstitialDelegate {
+    func interstitialDidLoad() {
+        NSLog("interstitialDidLoad")
+    }
+    
+    func interstitialDidFailToLoadWithError(_ error: Error!) {
+        NSLog("interstitialDidFailToLoadWithError")
+    }
+    
+    func interstitialDidOpen() {
+        
+    }
+    
+    func interstitialDidClose() {
+        
+    }
+    
+    func interstitialDidShow() {
+        
+    }
+    
+    func interstitialDidFailToShowWithError(_ error: Error!) {
+        
+    }
+    
+    func didClickInterstitial() {
+        
+    }
+    
     
 }
 extension ViewController:ISRewardedVideoDelegate{
     func rewardedVideoHasChangedAvailability(_ available: Bool) {
+        NSLog("trigger displayVideo")
         //
     }
     
     func didReceiveReward(forPlacement placementInfo: ISPlacementInfo!) {
+        NSLog("trigger displayVideo")
         //
     }
     
